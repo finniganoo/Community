f4196ab83809c2cdeaafe4156215d84c4ae79e88
Finnigan O <finnigan.o@mybazinga.com>
2019-04-18 16:50:27 -0700

    added IsPrime script

---
+++ MoreFunctions/MoreFunctions/prime.fsx
@@ -0,0 +1,21 @@
+﻿// Recursive isprime function.
+let isprime n =
+    let rec check i =
+        i > n/2 || (n % i <> 0 && check (i + 1))
+    check 2
+
+let aSequence = seq { for n in 1..10 do if isprime n then yield n }
+for x in aSequence do
+    printfn "%d" x
+
+
+
+// greatest common divisor
+
+open System
+
+let rec gcd x y =
+    if y = 0 then x
+    else gcd y (x % y)
+    
+Console.WriteLine(gcd 259 111) // prints 37
