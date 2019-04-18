702fd5f14245ab0e9fd80b24e4c00ca306c56fc4
Finnigan O <finnigan.o@mybazinga.com>
2019-04-17 17:29:55 -0700

    merge commit

--- MoreFunctions/MoreFunctions/polygons.fsx
+++ MoreFunctions/MoreFunctions/polygons.fsx
@@ -1,5 +1,4 @@
-﻿
-open System.Drawing
+﻿open System.Drawing
 open System.IO
 open System
 open System.Windows.Forms.VisualStyles
@@ -22,21 +21,21 @@ let naiveLine (x1, y1) (plotter: Plotter) =
         for x in x0..x1 do
             let proportion = float (x-x0) / xLen
             let y = int (Math.Round ( proportion * yLen )) + y0
-            printfn "%i" y
+            //printfn "%i" y
             plotter.bitmap.SetPixel (x,y,plotter.color)
     let x0,y0,x1,y1 = if y0 > y1 then x1,y1,x0,y0 else x0,y0,x1,y1
     if yLen <> 0.0 then 
         for y in y0..y1 do
             let proportion = float (y-y0) / yLen
             let x = int (Math.Round (proportion * xLen)) + x0
-            printfn "%i" x
+            //printfn "%i" x
             plotter.bitmap.SetPixel (x,y,plotter.color)
     updatedPlotter
 
 let turn amt plotter =
     let newDir = plotter.direction + amt
     let angled = { plotter with direction = newDir }
-    printfn "%A" angled
+    printfn "turn %A" angled.direction
     angled
 
 let move dist plotter =
@@ -48,23 +47,27 @@ let move dist plotter =
     let endX = (float startX) + (float dist) * cos rads
     let endY = (float startY) + (float dist) * sin rads
     let plottered = naiveLine (int endX, int endY) plotter
-    printfn "%A" plottered
+    printfn "draw %A pixel" dist
     plottered
 
 let pathAndFileName = 
     Path.Combine(__SOURCE_DIRECTORY__,"polygons.png")
 
-let bitmap = new Bitmap(100,100)
+let bitmap = new Bitmap(60,40)
 
 let initialPlotter = {
-    position = (20,35)
-    color = Color.Orange
-    direction = 90.0
+    position = (0,0)
+    color = Color.Red
+    direction = 0.0
     bitmap = bitmap
     }
 
+printfn "side margin: %A" (int ((bitmap.Width-60)/2))
+printfn "top/bottom margin: %A" (int ((bitmap.Height-30)/2))
+
 let rectangle x y = 
-    initialPlotter
+    { initialPlotter with position = (int ((bitmap.Width-x)/2), int ((bitmap.Height-y)/2) ) }
+    |> turn 90.0
     |> move x
     |> turn 90.0
     |> move y
@@ -73,7 +76,7 @@ let rectangle x y =
     |> turn 90.0
     |> move y
 
-let rect = rectangle 60 30
+let rect = rectangle 40 20
 
 rect.bitmap.Save(pathAndFileName)
 
